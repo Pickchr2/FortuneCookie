06dc4339c9182e688595350f922a146ffea12e4b
Chris Pickens <Cpickens1270@gmail.com>
2023-01-29 01:42:56 -0700

    Add project files.

---
+++ FortuneCookie/FortuneCookie.vb
@@ -0,0 +1,16 @@
+'Christopher Pickens
+'RCET0265
+'Spring 2023
+'Fortune Cookie
+'https://github.com/Pickchr2/FortuneCookie.git
+
+Option Explicit On
+Option Strict On
+
+Imports System
+
+Module FortuneCookie
+    Sub Main(args As String())
+
+    End Sub
+End Module
